11a638c8c35d81c64dad01cff555a5e50ee6cd7d
Kazunori <matunorigmnw@gmail.com>
2024-01-28 00:31:10 +0900

    debug

--- src/getTimezoneOffsetHours.test.js
+++ src/getTimezoneOffsetHours.test.js
@@ -1,4 +1,5 @@
 const getTimezoneOffsetHours = require('./getTimezoneOffsetHours');
 test('getTimezoneOffsetHours return -9', () => {
+  console.log(getTimezoneOffsetHours());
   expect(getTimezoneOffsetHours()).toBe(-9);
 })
